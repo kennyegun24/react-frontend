a8cad4f1912005ba86d910c51fa84b977cdf0e9d
jaamanu <kennyegun240@gmail.com>
2023-03-08 21:04:51 +0100

    App setup  and random messages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
